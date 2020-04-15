741ae492bbff4d03e9140e0cf48dd2c517100c9d
cbouman <34159271+cbouman@users.noreply.github.com>
2020-04-15 12:39:45 +0200

    Update dashapp2.py

--- dashapp2.py
+++ dashapp2.py
@@ -234,7 +234,7 @@ def update_graph2(yaxis_column_name2):
             margin= {'l': 50, 'r': 10, 'b': 50, 't': 50},
             hovermode= 'x',
             height= 400,
-            legend= dict(x=0.75, y=0.75))
+            legend= dict(x=0.9, y=0.9))
         }
 
 #%% Callback option based on hover over vakgroep timeseries
